255dd436ef43797124ebc7acb4b6677bde9051e9
Vladd11 <rozhkov.2006@gmail.com>
2022-03-24 20:37:26 +0400

    Correctly provided routing key & invalidated previous.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Server that runs FastAI prediction to differentiate road surface quality
+Based on https://github.com/thiagortk/Road-surface-detection-and-differentiation-considering-surface-damages
+
+Protocol: WebSocket (websockets library)
+
+Used in https://github.com/vladd11/Robot project, so this project also contains code to provide routing for client using [OpenRouteService](https://openrouteservice.org/).
--- main.py
+++ main.py
@@ -1,33 +1,29 @@
-ROUTING_KEY = '5b3ce3597851110001cf6248dff63f02cff84e50a0cbd265048be6d6'
 OUTPUT_PATH = '/home/rozhk/output'
-VIDEO_LATENCY = 10  # In seconds
+# PYTHONASYNCIODEBUG = True
+TARGET = (50.244607, 53.215471)
 
 import asyncio
-from websockets import serve
+import time
+
 import cv2
+import imutils
 import numpy
 import openrouteservice as openrouteservice
 from fastai.vision import *
+from websockets import serve
+import os
+from color import colorfull_fast, color_roads
 
-from color import colorfull_fast
-from reader import VideoCapture
+ROUTING_KEY = os.environ.get('ROUTING_KEY')
 
-os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "err_detect;aggressive|" \
-                                              "fflagsx;nobuffer|" \
-                                              "flags;low_delay|" \
-                                              "framedrop|" \
-                                              "fflags;strict|" \
-                                              "protocol_whitelist;file,rtp,udp"
-
+torch.set_num_threads(2)
 learner = load_learner('/home/rozhk')
-
-cam = VideoCapture("stream.sdp")
 client = openrouteservice.Client(key=ROUTING_KEY)
 
 
-def predict(frame_to_predict):
+def predict(frame):
     # noinspection PyTypeChecker
-    img = Image(pil2tensor(frame_to_predict, np.float32).div_(255))
+    img = Image(pil2tensor(frame, np.float32).div_(255))
     predicted = learner.predict(img)[0]
 
     predicted_width = predicted.shape[1] - 1
@@ -36,136 +32,52 @@ def predict(frame_to_predict):
     return predicted.data.numpy(), predicted_width, predicted_height
 
 
-shouldSendFrame = False
-imagePosValues = ()
+def get_center(prediction):
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7, 7))
+    opening = cv2.morphologyEx(color_roads(prediction.astype(
+        dtype=np.uint8)[0]), cv2.MORPH_OPEN, kernel, iterations=5)
+
+    contours = cv2.findContours(
+        opening, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours = imutils.grab_contours(contours)
+
+    cv2.imwrite('test.png', opening)
+
+    temp = []
+    for c in contours:
+        # compute the center of the contour
+        M = cv2.moments(c)
+        temp.append((int(M["m10"] / M["m00"]) / prediction.shape[2], int(M["m01"] / M["m00"]) / prediction.shape[1]))
+
+    return temp
 
 
 async def echo(websocket):
     async for message in websocket:
-        print('test')
+        if not isinstance(message, bytes):
+            message = message.split(' ')
+            message = message[1:3]
+            for i, e in enumerate(message):
+                message[i] = float(e)
+
+            routes = client.directions((message, TARGET),
+                                       optimize_waypoints=True, format='geojson')
+            await websocket.send('route ' + json.dumps(routes['features'][0]['geometry']['coordinates'][1:]))
+            await websocket.send('img')
+        else:
+            await websocket.send('img')
+            prediction, width, height = predict(
+                cv2.imdecode(np.frombuffer(message, np.uint8), cv2.IMREAD_COLOR))
+
+            cv2.imwrite(os.path.join(OUTPUT_PATH, f'{time.time()}.png'),
+                        colorfull_fast(prediction[0], numpy.empty(dtype=numpy.uint8, shape=(width, height, 3)), width,
+                                       height))
+            await websocket.send(json.dumps(get_center(prediction)))
 
 
 async def main():
-    async with serve(echo, "localhost", 8765):
+    async with serve(echo, "0.0.0.0", 8765):
         await asyncio.Future()
 
 
 asyncio.run(main())
-
-'''class Handler(BaseRequestHandler):
-    def handle(self):
-        ready = False
-        buffer = []
-        first = False
-
-        global shouldSendFrame
-        global imagePosValues
-
-        while True:
-            if first and not ready:
-                self.request.sendall(b'\x80')
-                ready = True
-            first = True
-            packet = self.request.recv(1024)
-            if ready:
-                if packet == b'j':
-                    ready = False
-                    prediction, width, height = predict(
-                        cv2.imdecode(np.frombuffer(bytes(buffer), np.uint8), cv2.IMREAD_COLOR))
-                    cv2.imwrite('test.png', colorfull_fast(prediction[0], numpy.empty(
-                        dtype=numpy.uint8, shape=(width, height, 3)), width, height))
-
-                    buffer = []
-                else:
-                    buffer += packet
-            else:
-                if packet == b"b":
-                    while not imagePosValues:
-                        shouldSendFrame = True
-
-                    if imagePosValues is None:
-                        print("imagePosValues is null")
-                        return
-
-                    # means road is straight
-                    self.request.sendall(struct.pack('%sf' % len(imagePosValues), *imagePosValues))
-                    imagePosValues = ()
-                elif packet.startswith(b"r"):
-                    json_packet = json.loads(packet[1:])
-                    routes = client.directions((json_packet[u'c'], json_packet[u'e']),
-                                               optimize_waypoints=True, format='geojson')
-                    print(json.dumps(routes['features'][0]['geometry']['coordinates'][1:]))
-                    self.request.sendall(
-                        json.dumps(routes['features'][0]['geometry']['coordinates'][1:]).encode("utf-8") + b'\n')
-                elif packet == b"c":
-                    return
-
-
-def run():
-    with TCPServer(("0.0.0.0", 1071), Handler) as server:
-        server.serve_forever()
-'''
-
-'''class ImageHandler(BaseRequestHandler):
-    def __init__(self, request, client_address, server):
-        super().__init__(request, client_address, server)
-        self.buffer = None
-
-    def handle(self):
-        self.request.settimeout(1000)
-        while True:
-            try:
-                packet = self.request.recv(1024).strip()
-            except socket.timeout:
-                if self.buffer != None:
-                    prediction, width, height = predict(cv2.imread())
-
-                    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7, 7))
-                    opening = cv2.morphologyEx(color_roads(prediction.astype(
-                        dtype=np.uint8)[0]), cv2.MORPH_OPEN, kernel, iterations=5)
-
-                    contours = cv2.findContours(
-                        opening, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-                    contours = imutils.grab_contours(contours)
-
-                    cv2.imwrite('test.png', opening)
-
-                    if len(contours) == 1:
-                        M = cv2.moments(contours[0])
-                        cX = int(M["m10"] / M["m00"])
-                        cY = int(M["m01"] / M["m00"])
-
-                        imagePosValues = (cX / prediction.shape[2], cY / prediction.shape[1])
-                    else:
-                        temp = []
-                        for c in contours:
-                            # compute the center of the contour
-                            M = cv2.moments(c)
-                            cX = int(M["m10"] / M["m00"])
-                            cY = int(M["m01"] / M["m00"])
-
-                            print(cX / prediction.shape[2], cY / prediction.shape[1])
-
-                            temp.append(cX / prediction.shape[2])
-                            temp.append(cY / prediction.shape[1])
-                        imagePosValues = temp
-
-                    shouldSendFrame = False
-                self.buffer = None
-            else:
-                self.buffer += packet
-
-while True:
-    frame = cam.read()
-    if shouldSendFrame:
-        print('Receiving last frame')
-        sleep(VIDEO_LATENCY)  # TODO: Bad way to get round the latency
-
-        
-    elif frame is not None:
-        prediction, width, height = predict(frame)
-        # because it works well
-        # noinspection PyTypeChecker
-        cv2.imwrite(os.path.join(OUTPUT_PATH, f'{round(time() * 1000)}.png'), colorfull_fast(prediction[0], numpy.empty(
-            dtype=numpy.uint8, shape=(width, height, 3)), width, height))
-'''
